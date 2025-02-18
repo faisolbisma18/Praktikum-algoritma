af89046dd3e07bac671cfe675a3d44431ec5cd61
faisolbisma18 <144082490+faisolbisma18@users.noreply.github.com>
2025-02-18 13:33:03 +0700

    Add files via upload

--- MahasiswaMain10.java
+++ MahasiswaMain10.java
@@ -10,6 +10,13 @@ public class MahasiswaMain10 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
-    }
+
+        Mahasiswa10 mhs2 = new Mahasiswa10("Annisa Nabila", "2141720160", "TI 2L", 3.25);
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
+
+        Mahasiswa10 mhsBisma = new Mahasiswa10("Mochammad Faisol Bismagraha", "244107020010", "TI 1F",3.55);
+        mhsBisma.tampilkanInformasi();
+        }
     
 }

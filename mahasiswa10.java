2f4ab770d431ea4c7e758b8e77555ea9246c9243
faisolbisma18 <144082490+faisolbisma18@users.noreply.github.com>
2025-02-18 12:00:18 +0700

    Add files via upload

---
+++ mahasiswa10.java
@@ -0,0 +1,54 @@
+class Mahasiswa10 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    public Mahasiswa10(String nama, String nim, String kelas, double ipk) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        this.ipk = ipk;
+    }
+
+    public void tampilkanInformasi() {
+        System.out.println("Nama  : " + nama);
+        System.out.println("NIM   : " + nim);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+        System.out.println("Kinerja: " + nilaiKinerja());
+        System.out.println("============================");
+    }
+
+    public void ubahKelas(String kelasBaru) {
+        this.kelas = kelasBaru;
+    }
+
+    public void updateIpk(double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            this.ipk = ipkBaru;
+            System.out.println("IPK berhasil diperbarui menjadi: " + ipk);
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
+    }
+    
+
+    public String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+}
+
+public class mahasiswa10 {
+    
+
+    }
+
